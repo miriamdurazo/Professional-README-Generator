6e460eb2931838c85981da37005640da8fd1858d
miriamdurazo <md13707@my.bristol.ac.uk>
2023-02-27 22:03:26 +0000

    put questions in an array separated from the writefile function

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 import inquirer from 'inquirer';
-import fs from 'fs';
+import * as fs from 'fs';
 
-prompt([
-    {
-      type: 'input',
-      name: 'project_title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      message: 'Description',
-      name: 'description',
-    },
-    {
-      type: 'input',
-      message: 'What do you want to include in the table of contents?',
-      name: 'table_of_contents',
-    },
+// array of questions for user
+const questions = [
     {
         type: 'input',
-        message: 'What are the installation instructions?',
-        name: 'installation',
-    },
-    {
+        name: 'project_title',
+        message: 'What is the title of your project?',
+      },
+      {
         type: 'input',
-        message: 'Enter your GitHub username:',
-        name: 'user_name',
-    },
-    {
+        message: 'Description',
+        name: 'description',
+      },
+      {
         type: 'input',
-        message: 'Enter the link to your GitHub profile:',
-        name: 'profile',
-    },
-    {
-        type: 'input',
-        message: 'Enter your email address:',
-        name: 'email',
-    }
-  ])
-  .then((data) => {
+        message: 'What do you want to include in the table of contents?',
+        name: 'table_of_contents',
+      },
+      {
+          type: 'input',
+          message: 'What are the installation instructions?',
+          name: 'installation',
+      },
+      {
+          type: 'input',
+          message: 'Enter your GitHub username:',
+          name: 'user_name',
+      },
+      {
+          type: 'input',
+          message: 'Enter the link to your GitHub profile:',
+          name: 'profile',
+      },
+      {
+          type: 'input',
+          message: 'Enter your email address:',
+          name: 'email',
+      }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    inquirer.prompt(questions)
+    .then((data) => {
     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
     // arguments for hte writeFunction = filename, text to be written error function
-    writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
